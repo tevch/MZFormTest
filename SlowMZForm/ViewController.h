627a7170be6e85ace20b2d2c20a7ed6d7ee7a316
Andrey Kan <list@livewings.com>
2014-04-11 01:37:06 -0400

    add tdpicker. based on MZForm

--- SlowMZForm/ViewController.h
+++ SlowMZForm/ViewController.h
@@ -10,4 +10,6 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)showMZForm:(id)sender;
+
 @end
--- SlowMZForm/ViewController.m
+++ SlowMZForm/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "TDPickerController.h"
 
 @interface ViewController ()
 
@@ -24,6 +25,22 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showMZForm:(id)sender {
+    NSLog(@"showMZForm");
+    
+    NSMutableArray * patternValues = [[NSMutableArray alloc] init];
+    for(int altitude = 100; altitude<1500;altitude+=50) {
+        [patternValues addObject: [NSString stringWithFormat:@"%d", altitude]];
+    }
+    
+    TDPickerController* picker = [[TDPickerController alloc] init];
+    picker.pickerTitle = @"Select Turn Altitude";
+    picker.pickerData = patternValues;
+    picker.selectedValue = [NSString stringWithFormat:@"%d", 500];
+    
+    [picker showDialog];
 }
 
 @end
