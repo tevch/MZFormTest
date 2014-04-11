f3e82ab32f19825d0aa9ea44d3aec94bd5328a02
Andrey Kan <list@livewings.com>
2014-04-11 01:20:20 -0400

    Initial Commit

---
+++ SlowMZForm/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlowMZForm
+//
+//  Created by Andrey Kan on 4/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SlowMZForm/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SlowMZForm
+//
+//  Created by Andrey Kan on 4/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
